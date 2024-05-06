5d333fa119c531617bddd81a915db6e11457e731
Abdul Mannan <abdulmann1234@gmail.com>
2024-05-06 12:55:41 +0500

    Time: 4 ms (26.35%) | Memory: 7.5 MB (78.74%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int i = 0;
+
+        while(i < word1.length() && i < word2.length()) {
+            result += word1[i];
+            result += word2[i];
+            i++;
+        }
+
+        while(word1.length() > word2.length() && i < word1.length()) {
+            result += word1[i];
+            i++;
+        }
+
+        while(word1.length() < word2.length() && i < word2.length()) {
+            result += word2[i];
+            i++;
+        }
+
+        return result;
+    }
+};
