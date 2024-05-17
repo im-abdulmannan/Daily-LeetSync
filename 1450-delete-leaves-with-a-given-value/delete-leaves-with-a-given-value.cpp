0cf8572d1716c0005eacdcdc1050e657a46f3b9a
Abdul Mannan <abdulmann1234@gmail.com>
2024-05-17 21:47:19 +0500

    Time: 11 ms (64.37%) | Memory: 21 MB (100.00%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -6,16 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        if(!root) return nullptr;
+        if (!root)
+            return nullptr;
+
         root->left = removeLeafNodes(root->left, target);
         root->right = removeLeafNodes(root->right, target);
-        if(!root->left && !root->right && root->val == target) return nullptr;
+
+        if (!root->left && !root->right && root->val == target)
+            return nullptr;
+
         return root;
     }
 };
