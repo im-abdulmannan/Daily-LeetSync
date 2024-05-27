6d2d78cf87676ab3daafad233cedded11b8d295a
Abdul Mannan <bsf2100835@ue.edu.pk>
2024-05-27 15:16:26 +0500

    Time: 3 ms (45.69%) | Memory: 11 MB (5.22%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,22 +3,26 @@ public:
     string mergeAlternately(string word1, string word2) {
         string result = "";
         int i = 0;
+        int j = 0;
 
-        while(i < word1.length() && i < word2.length()) {
-            result += word1[i];
-            result += word2[i];
-            i++;
+        while(i < word1.length() && j < word2.length()) {
+            result = result + word1[i]; 
+            result = result + word2[j]; 
+            i+=1, j+=1; // i -> 3, j -> 3
         }
+        // apbqcr
 
         while(word1.length() > word2.length() && i < word1.length()) {
-            result += word1[i];
+            result = result + word1[i];
             i++;
         }
+        // apbqcr -- tf from class 1
 
-        while(word1.length() < word2.length() && i < word2.length()) {
-            result += word2[i];
-            i++;
+        while(word2.length() > word1.length() && j < word2.length()) {
+            result += word2[j];
+            j++;
         }
+        // apbqcr -- tf from class 2
 
         return result;
     }
