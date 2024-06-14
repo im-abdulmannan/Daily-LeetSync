47da61678b3a5995ad91d0a81321b0f75233e2eb
Abdul Mannan <bsf2100835@ue.edu.pk>
2024-06-14 15:36:50 +0500

    Time: 962 ms (5.00%) | Memory: 28.5 MB (64.24%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -7,8 +7,27 @@ public:
             result[i] = nums[i] * nums[i];
         }
 
-        sort(result.begin(), result.end());
+        selectionSort(result);
 
         return result;
+    }
+
+private:
+    void selectionSort(vector<int>& arr) {
+        int n = arr.size();
+
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the remaining unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }
+
+            // Swap the found minimum element with the first element
+            cout << arr[min_idx] << " " << arr[i] << endl;
+            swap(arr[min_idx], arr[i]);
+        }
     }
 };
