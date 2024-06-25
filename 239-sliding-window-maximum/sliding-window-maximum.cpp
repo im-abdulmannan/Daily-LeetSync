2d37da28da9e01dbf7d3584fde3ac8b8ccf0dad2
Abdul Mannan <bsf2100835@ue.edu.pk>
2024-06-25 18:43:11 +0500

    Time: 170 ms (65.31%) | Memory: 139.4 MB (31.17%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -3,17 +3,20 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> result;
         deque<pair<int, int>> dq;
-        
+
         for(int i = 0; i < nums.size(); i++) {
-            if(!dq.empty() && dq.front().second <= (i - k)) {
+            if(!dq.empty() && dq.front().second <= (i - k))
+            {
                 dq.pop_front();
             }
 
-            while(!dq.empty() && dq.back().first < nums[i]) {
+            while(!dq.empty() && dq.back().first < nums[i])
+            {
                 dq.pop_back();
             }
+
             dq.push_back(make_pair(nums[i], i));
-            
+
             if(i >= k - 1) {
                 result.push_back(dq.front().first);
             }
