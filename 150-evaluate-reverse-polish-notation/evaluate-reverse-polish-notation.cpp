bf368d9ce71dcff4cd6d1f29b938f5b32ef57b76
Abdul Mannan <bsf2100835@ue.edu.pk>
2024-06-29 16:48:36 +0500

    Time: 4 ms (89.66%) | Memory: 15.3 MB (86.12%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> s;
-        for (int i =0;i <tokens.size(); i++){
-            if(tokens[i] != "+"&& tokens[i] != "-"&& tokens[i] != "*"&& tokens[i] != "/"){
-                s.push(stoi(tokens[i]));
-            }else if(tokens[i] == "+"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b+a);
-            }else if(tokens[i] == "-"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b-a);
-            }else if(tokens[i] == "*"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b*a);
-            }else if(tokens[i] == "/"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b/a);
+        stack<int> stk;
+
+        for(int i = 0; i < tokens.size(); i++) {
+            if(tokens[i] != "+" && tokens[i] != "*" && tokens[i] != "-" && tokens[i] != "/") {
+                stk.push(stoi(tokens[i]));
+            }
+            else
+            {
+                int operand1 = stk.top();
+                stk.pop();
+                int operand2 = stk.top();
+                stk.pop();
+                if(tokens[i] == "+") {
+                    stk.push(operand2 + operand1);
+                }
+                else if(tokens[i] == "-") {
+                    stk.push(operand2 - operand1);
+                }
+                else if(tokens[i] == "*") {
+                    stk.push(operand2 * operand1);
+                }
+                else if(tokens[i] == "/") {
+                    stk.push(operand2 / operand1);
+                }
             }
         }
-       return s.top() ;
+
+        return stk.top();
     }
 };
