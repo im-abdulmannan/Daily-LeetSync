ee6be960000c71d1807515f9779869048945a7fc
Abdul Mannan <a.mannan0607@gmail.com>
2024-07-25 03:06:15 +0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (13.04%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,13 +11,8 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) {
-            return list2;
-        }
-
-        if(!list2) {
-            return list1;
-        }
+        if(!list2) return list1;
+        if(!list1) return list2;
 
         if(list1->val > list2->val) {
             swap(list1, list2);
