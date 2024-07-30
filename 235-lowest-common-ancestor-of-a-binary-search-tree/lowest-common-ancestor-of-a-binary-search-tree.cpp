7dcf08655a9e2fbde50b21e8f1127cb3eaefc823
Abdul Mannan <a.mannan0607@gmail.com>
2024-07-30 14:42:27 +0500

    Time: 25 ms (42.31%) | Memory: 22.1 MB (22.89%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,15 +11,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root->val == p->val || root->val == q->val) return root;
+        if(p->val == root->val || q->val == root->val) return root;
 
-        if(p->val < root->val && q->val > root->val) return root;
         if(p->val > root->val && q->val < root->val) return root;
-
+        if(p->val < root->val && q->val > root->val) return root;
+        
         if(p->val < root->val && q->val < root->val)
             return lowestCommonAncestor(root->left, p, q);
-        
-        else
-            return lowestCommonAncestor(root->right, p, q);
+
+        else return lowestCommonAncestor(root->right, p, q);
     }
 };
