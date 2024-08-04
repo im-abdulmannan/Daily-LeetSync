5807c623288c744502c7dfd03a1079e463067399
Abdul Mannan <a.mannan0607@gmail.com>
2024-08-04 16:24:30 +0500

    Time: 78 ms (80.67%) | Memory: 13.5 MB (75.95%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        // sort(nums.begin(), nums.end());
-        vector<int> result;
-        for(int i = 0; i < n; i++) {
+        int sumOfSubArr[n * (n + 1) / 2];
+        for(int i = 0, k = 0; i < n; i++) {
             int sum = 0;
             for(int j = i; j < n; j++) {
                 sum += nums[j];
-                result.push_back(sum);
+                sumOfSubArr[k++] = sum;
             }
         }
-        sort(result.begin(), result.end());
-        // for(int re : result) cout << re << " ";
+
+        sort(sumOfSubArr, sumOfSubArr + (n * (n + 1) / 2));
         int ans = 0;
         const int MOD = 1e9 + 7;
         for(int i = left - 1; i < right; i++) {
-            ans = (ans + result[i]) % MOD;
+            ans = (ans + sumOfSubArr[i]) % MOD;
         }
 
         return ans;
