8004d7f48d92c5922e4fa013b29ee9597ddd67e8
Abdul Mannan <a.mannan0607@gmail.com>
2024-08-13 16:56:46 +0500

    Time: 4 ms (60.85%) | Memory: 12.9 MB (38.57%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> result;
-        vector<int> current;
+        vector<int> curr;
         sort(candidates.begin(), candidates.end());
-
-        backtrack_helper(result, candidates, current, target, 0);
+        helper(candidates, result, curr, target, 0);
         return result;
     }
 
-    void backtrack_helper(vector<vector<int>>& result, vector<int>& candidates, vector<int>& current, int target, int start) {
+    void helper(vector<int>& candidates, vector<vector<int>>& result, vector<int>& curr, int target, int start) {
         if(target == 0) {
-            result.push_back(current);
+            result.push_back(curr);
             return;
         }
-
+       
         for(int i = start; i < candidates.size(); i++) {
             if(candidates[i] <= target) {
-                current.push_back(candidates[i]);
-                backtrack_helper(result, candidates, current, target - candidates[i], i + 1);
-                while(i < candidates.size() - 1 && candidates[i] == candidates[i+1]) i++;
-                current.pop_back();
+                curr.push_back(candidates[i]);
+                helper(candidates, result, curr, target - candidates[i], i + 1);
+                while(i < candidates.size() - 1 && candidates[i] == candidates[i + 1]) i++;
+                curr.pop_back();
             }
         }
     }
