f74adaff641d0d51c632fe800f242f263b97c578
Abdul Mannan <a.mannan0607@gmail.com>
2024-08-15 16:15:45 +0500

    Time: 75 ms (43.55%) | Memory: 86 MB (50.86%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+
+        for (auto bill : bills) {
+            if(bill == 5) five++;
+            else if (bill == 10) {
+                if(five == 0) return false;
+                five--;
+                ten++;
+            } else if (bill == 20) {
+                if(five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if(five > 2) {
+                    five -= 3;
+                } else return false;
+            }
+        }
+
+        return true;
+    }
+};
