bdd6c5b541c81061a1c1146f32f968ceca61c136
Abdul Mannan <a.mannan0607@gmail.com>
2024-09-05 23:29:01 +0500

    Time: 76 ms (97.06%) | Memory: 116.4 MB (69.12%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -4,24 +4,18 @@ public:
         ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
         int m = rolls.size(), sum = 0;
-        for (auto roll : rolls) {
-            sum += roll;
-        }
+        for (auto roll : rolls) sum += roll;
 
         int rhs = mean * (n + m);
         rhs -= sum;
         int x = rhs / n, y = rhs % n;
-        if (x <= 0 || x > 6)
-            return {};
-        if (x == 6 && y > 0)
-            return {};
+        if (x <= 0 || x > 6) return {};
+        if (x == 6 && y > 0) return {};
+
         vector<int> ans(n);
-        for (int i = 0; i < n; i++) {
-            ans[i] = x;
-        }
-        for (int i = 0; i < y; i++) {
-            ans[i]++;
-        }
+        for (int i = 0; i < n; i++) ans[i] = x;
+        for (int i = 0; i < y; i++) ans[i]++;
+
         return ans;
     }
 };
