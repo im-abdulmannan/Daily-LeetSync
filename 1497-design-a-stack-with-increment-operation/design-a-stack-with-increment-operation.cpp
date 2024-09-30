651bced54c3a12b5746c111ec76debe05944576c
Abdul Mannan <a.mannan0607@gmail.com>
2024-09-30 15:20:35 -0700

    Time: 38 ms (8.75%) | Memory: 26 MB (67.11%) - LeetSync

---
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,34 @@
+class CustomStack {
+public:
+    int MaxSize;
+    deque<int> dq;
+    CustomStack(int maxSize) { MaxSize = maxSize; }
+
+    void push(int x) {
+        if (dq.size() < MaxSize)
+            dq.push_back(x);
+    }
+
+    int pop() {
+        int val = -1;
+        if (dq.size()) {
+            val = dq.back();
+            dq.pop_back();
+        }
+        return val;
+    }
+
+    void increment(int k, int val) {
+        int n = min(k, int(dq.size()));
+        for (int i = 0; i < n; i++)
+            dq[i] += val;
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
